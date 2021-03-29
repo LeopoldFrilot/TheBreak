49de2339f6fec3b0f4c21ffcf929a5ab34428de8
Leopold Frilot <LAFrilot@outlook.com>
2021-03-29 16:25:24 -0500

    Implemented music and background color changes

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField] Color[] backgrounds;
+    [SerializeField] float[] timeBreaks;
+    [Header("For viewing purposes")]
+    [SerializeField] bool started = false;
+    [SerializeField] int curBackground;
+    [SerializeField] int curTimeBreak;
+    [SerializeField] float curTime;
+
+    private void Update()
+    {
+        if(started)
+        {
+            curTime += Time.deltaTime;
+            if (curTime >= timeBreaks[curTimeBreak])
+            {
+                Camera.main.backgroundColor = backgrounds[curBackground];
+                curBackground += 1 % backgrounds.Length;
+                curTimeBreak++;
+                if (curTimeBreak >= timeBreaks.Length) started = false;
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        started = true;
+        curTime = 0;
+        curBackground = 0;
+        curTimeBreak = 0;
+    }
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public void RestartAllTracks()
+    {
+        GetComponent<AudioSource>().Stop();
+        int numChildren = transform.childCount;
+        for(int i = 0; i < numChildren; i++)
+        {
+            AudioSource audioSource = transform.GetChild(i).GetComponent<AudioSource>();
+            audioSource.Stop();
+            audioSource.Play();
+        }
+        var BC = FindObjectOfType<BackgroundController>();
+        BC.Reset();
+    }
+}
